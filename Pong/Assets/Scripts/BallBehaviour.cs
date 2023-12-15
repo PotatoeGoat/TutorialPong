737304cbc39bbf1084313d2ddb1151d54eb80853
PotatoeGoat <147033284+PotatoeGoat@users.noreply.github.com>
2023-12-15 08:09:38 +0000

    add1

---
+++ Pong/Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+
+
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 direction;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Random.Range(0.0f,1.0f)<0.5f)
+        {
+            
+            direction = Vector3.right;
+        }
+        else
+        {
+            
+            direction = Vector3.left;
+        }
+
+        if (Random.Range(0.0f, 1.0f) < 0.5f)
+        {
+            
+            direction += Vector3.up;
+        }
+        else
+        {
+           
+            direction += Vector3.down;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * 3 * Time.deltaTime;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("colision con " + collision.gameObject.name);
+
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("cambiar direccion en eje x");
+            direction.x = -direction.x;
+
+            Debug.Log("cambiar direccion en eje y");
+            //direction.y = -direction.y;
+        }
+
+        if (collision.gameObject.CompareTag("Border"))
+        {
+            Debug.Log("cambiar direccion en eje x");
+            //direction.x = -direction.x;
+
+            Debug.Log("cambiar direccion en eje y");
+            direction.y = Random.Range()
+        }
+    }
+
+}
---
+++ Pong/Assets/Scripts/MiPrimeraEscene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiPrimeraEscene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Va a empezar a moverse");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
